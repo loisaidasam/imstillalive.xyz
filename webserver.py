3f0f3370476203f2c0f3584d8be39d842542e7d1
Sam Sandberg <sam.sandberg@gmail.com>
2017-11-07 14:20:23 -0500

    Custom expiration

--- webserver.py
+++ webserver.py
@@ -1,8 +1,12 @@
 
 import time
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+
+KEY_EXPIRES_SECONDS = 'expires_s'
+KEY_EXPIRES_MINUTES = 'expires_m'
+KEY_EXPIRES_HOURS = 'expires_h'
 
 # DEFAULT_EXPIRATION = 10  # 10 sec
 # DEFAULT_EXPIRATION = 60 * 10  # 10 min
@@ -27,13 +31,27 @@ def ping_put(thing):
     return "%s %s" % (thing, now)
 
 
+def _get_expiration():
+    seconds = request.args.get(KEY_EXPIRES_SECONDS)
+    if seconds:
+        return int(seconds)
+    minutes = request.args.get(KEY_EXPIRES_MINUTES)
+    if minutes:
+        return int(minutes) * 60
+    hours = request.args.get(KEY_EXPIRES_HOURS)
+    if hours:
+        return int(hours) * 60 * 60
+    return DEFAULT_EXPIRATION
+
+
 @app.route('/ping/<thing>', methods=['GET'])
 def ping_get(thing):
     if thing not in last_ping:
         response = u"No such thing as %s" % thing
         return response, 404
     elapsed = time.time() - last_ping[thing]
-    if elapsed > DEFAULT_EXPIRATION:
+    expiration = _get_expiration()
+    if elapsed > expiration:
         del last_ping[thing]
         response = u"Thing expired: %s" % thing
         return response, 404
